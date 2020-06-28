00a20e73d5d6ad4953f6d85f8edbc2a749e9826b
desmond <stepashka1999@mail.ru>
2020-06-28 22:56:48 +0700

    Добавьте файлы проекта.

---
+++ HomeWork_4/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var month = 12;
+
+            Random rnd = new Random();
+
+            var pattern = $"{"Месяц",8} {"Доходы, тыс. руб.",20} {"Расходы, тыс. руб",20} {"Прибыль, тыс. руб.",20}";
+
+            var rashodi = new int[month];
+            var dohodi = new int[month];
+            var pribil = new int[month];
+
+            var minValues = new int[3];
+
+            //Заполняем массив
+            for (int i = 0; i < month; i++)
+            {
+                dohodi[i] = rnd.Next(7, 20) * 10_000;
+                rashodi[i] = rnd.Next(7, 15) * 10_000;
+
+                pribil[i] = dohodi[i] - rashodi[i];
+            }
+
+
+
+            Console.WriteLine(pattern);
+
+            //Выводим массив на экран
+            for (int i = 0; i < month; i++)
+            {
+                Console.WriteLine($"{i+1, 6}{dohodi[i],19}{rashodi[i],19}{pribil[i],19}");
+            }
+
+            //заполнение максимальными значениями
+            for(int i = 0; i < minValues.Length; i++)
+            {
+                minValues[i] = pribil.Max();
+            }
+
+            //Поиск 3х уникальных минимальных значений
+            minValues[0] = pribil.Min();
+            minValues[1] = pribil.Max();
+            minValues[2] = pribil.Max();
+
+            for(int i = 1; i < minValues.Length; i++)
+            {
+                for(int j = 0; j < pribil.Length; j++)
+                {
+                    if (pribil[j] > minValues[i - 1] && minValues[i] > pribil[j])
+                    {
+                        minValues[i] = pribil[j];
+                    }
+                }
+            }
+
+            //поиск месяцев с худшим доходом
+            int[] badMonth = new int[12];
+
+            for(int i = 0; i < pribil.Length; i++)
+            {
+                badMonth[i] = 0;
+                for (int j = 0; j < minValues.Length; j++)
+                {
+                    if(pribil[i] == minValues[j]) badMonth[i] = 1;
+                }
+            }
+
+            //Вывод месяцев с худшим доходом
+            Console.WriteLine();
+            Console.Write("Худшая прибыль в месяцах: ");
+            for(int i = 0; i < badMonth.Length; i++)
+            {
+                if (badMonth[i] == 1) Console.Write(i+1 + ", ");
+            }
+            Console.CursorLeft-=2;
+            Console.Write(" ");
+            Console.WriteLine();
+
+
+            //Поиск и вывод месяцев с положительнывм доходом
+            var countOfGoodMonth = 0;
+            Console.Write("Полижительная прибыль в месяцах: ");
+            for (int i = 0; i < pribil.Length; i++)
+            {
+                if (pribil[i] > 0)
+                {
+                    Console.Write(i + 1 + ", ");
+                    countOfGoodMonth++;
+                }
+            }
+            Console.CursorLeft -= 2;
+            Console.Write($" : {countOfGoodMonth} мес.");
+            Console.WriteLine();
+
+        }
+    }
+}
---
+++ HomeWork_4_Task_2/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork_4_Task_2
+{
+    class Program
+    {
+        //В задаче 2 неверный пример. Как я понял из формулы. треугольник при n=9 будт иметь 10 строк, т.е n+1. Т.к.  А в примере при n=9 треугольник имеет 8 строк.
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите количество");
+            var N = int.Parse(Console.ReadLine());
+
+            var paskalArray = new int[N, N];
+
+            FillPascalArray(paskalArray);
+
+            Console.WriteLine("Простой вариант: \n");
+
+            for (int k = 0; k < N; k++)
+            {
+                for (int i = 0; i <= k; i++)
+                {
+                    Console.Write($"{paskalArray[k - i, i], 10}");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("Вариант посложнее: \n");
+
+            for (int k = 0; k < N; k++)
+            {
+                Console.Write(new string(' ', N - k));
+                for (int i = 0; i <= k; i++)
+                {
+                    Console.Write($" {paskalArray[k - i, i], 1}");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        static void FillPascalArray(int[,] array)
+        {
+            var size = array.GetLength(0);
+
+            for(int i = 0; i < size; i++)
+                for(int j = 0; j < size; j++)
+                {
+                    if (i == 0 || j == 0) array[i, j] = 1;
+                    else
+                    {
+                        array[i, j] = array[i-1, j] + array[i, j - 1];
+                    }
+                }
+        }
+
+    }
+}
